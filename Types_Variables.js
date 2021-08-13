83689fd32dcd6477e25e24ae9f21a47d152f2dd5
Blue <leanefelis@gmail.com>
2021-08-13 12:50:58 +0200

    Modifs + ajouts

---
+++ Types_Variables.js
@@ -0,0 +1,19 @@
+// Virgule Flottante
+let integerCalculation = 1 + 2;  // donne 3
+let weirdCalculation = 0.1 + 0.2;  // on attend 0.3, réponse réelle 0.30000000000000004
+
+// Valeurs Logiques // Bolean
+let userIsSignedIn = true;
+let userIsAdmin = false;
+
+// Chaînes de caractères
+let firstName = "Will";
+let lastName = 'Alexander';
+
+// Concaténation
+let wholeName = firstName + " " + lastName;  // valeur: "Will Alexander"
+
+const myName = `Alexander`;
+const salutation = `Bienvenue sur mon site ${myName}!`;
+console.log(salutation);   //retournera “Bienvenue sur mon site Alexander!” 
+
