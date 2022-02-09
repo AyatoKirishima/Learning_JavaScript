5028713e03eb29020260e867aff1f50f46e83d1b
Blue <leanefelis@gmail.com>
2022-02-09 09:01:26 +0100

    ex1 ok ?

--- TD02/ex01.js
+++ TD02/ex01.js
@@ -8,7 +8,21 @@ const mainDiv = document.querySelector("#main-div");
 // }
 
 const appendDiv = () => {
-  mainDiv.innerHTML = mainDiv.innerHTML + "<div>Appended div!</div>";
+  // create a new div element
+  const newDiv = document.createElement("div");
+
+  // and give it some content
+  const newContent = document.createTextNode("Oy oy oy");
+
+  // add the text node to the newly created div
+  newDiv.appendChild(newContent);
+
+  // add the newly created element and its content into the DOM
+  const currentDiv = document.getElementById("main-div");
+  // document.body.insertBefore(newDiv, currentDiv); 
+  $("div").append("Hello");
+  // Permet de remplacer le texte de la 'main-div'
+  // document.getElementById('main-div').textContent = 'oy oy oy';
 };
 
 mainDiv.querySelector("button").addEventListener("click", appendDiv);
