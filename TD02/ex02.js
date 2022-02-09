d70d6604487634952686d3dcef8c117eabfe96cc
Blue <leanefelis@gmail.com>
2022-02-09 09:40:18 +0100

    ex02
    ex02

--- TD02/ex02.js
+++ TD02/ex02.js
@@ -1,13 +1,16 @@
 "use strict";
 
 document.querySelector("main").addEventListener("click", (event) => {
-  logEvent("Click event caught on MAIN");
+  logEvent("Click event caught on MAIN"); //inutile de stopper la propagation : il n'y a qu'un élément
 });
 
 document.querySelector("#outer-div").addEventListener("click", (event) => {
   logEvent("Click event caught on DIV");
+  // event.preventDefault();
+  event.stopPropagation();
 });
 
 document.querySelector("#main-button").addEventListener("click", (event) => {
   logEvent("Click event caught on BUTTON");
+  event.stopPropagation();
 });
