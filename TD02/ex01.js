65845e1c8031fcdc40fdd23dfe83de2f2c70189d
Blue <leanefelis@gmail.com>
2022-02-09 09:18:32 +0100

    Update ex01.js
    correction

--- TD02/ex01.js
+++ TD02/ex01.js
@@ -2,7 +2,7 @@
 
 const mainDiv = document.querySelector("#main-div");
 
-// Adapter sans utiliser innerHTML
+// Adapter sans utiliser innerHTML ou toucher au HTML
 // const appendDiv = () => {
 //   mainDiv.innerHTML = mainDiv.innerHTML + "<div>Appended div!</div>";
 // }
@@ -12,17 +12,28 @@ const appendDiv = () => {
   const newDiv = document.createElement("div");
 
   // and give it some content
-  const newContent = document.createTextNode("Oy oy oy");
+  const newContent = document.createTextNode("Appended div !");
 
   // add the text node to the newly created div
   newDiv.appendChild(newContent);
 
   // add the newly created element and its content into the DOM
   const currentDiv = document.getElementById("main-div");
-  // document.body.insertBefore(newDiv, currentDiv); 
-  $("div").append("Hello");
+  // document.body.insertBefore(newDiv, currentDiv); insere le texte avant la div
+
+  $("div#main-div").append(newDiv); //ajouter le texte à la div
+  // JQUERY (à éviter)
+  // $  pour toutes les div qui ont la classe "main-div"
+
   // Permet de remplacer le texte de la 'main-div'
   // document.getElementById('main-div').textContent = 'oy oy oy';
 };
 
 mainDiv.querySelector("button").addEventListener("click", appendDiv);
+
+// Correction (sans jquery)
+// const appendDiv = () => {
+//   const newDiv = document.createElement("div");
+//   newDiv.textContent = "Appended div !";
+//   mainDiv.appendChild(newDiv);
+// }
