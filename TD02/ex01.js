caa8d860693ef7c4485697f4bf106c8e71fd1f01
Blue <leanefelis@gmail.com>
2022-02-09 08:45:19 +0100

    analyse

--- TD02/ex01.js
+++ TD02/ex01.js
@@ -2,6 +2,11 @@
 
 const mainDiv = document.querySelector("#main-div");
 
+// Adapter sans utiliser innerHTML
+// const appendDiv = () => {
+//   mainDiv.innerHTML = mainDiv.innerHTML + "<div>Appended div!</div>";
+// }
+
 const appendDiv = () => {
   mainDiv.innerHTML = mainDiv.innerHTML + "<div>Appended div!</div>";
 };
