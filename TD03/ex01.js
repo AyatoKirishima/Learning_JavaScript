51d095d0dc16111af3609c850d06b3daa60cde69
Blue <leanefelis@gmail.com>
2022-03-02 09:41:37 +0100

    :o selector :o:

--- TD03/ex01.js
+++ TD03/ex01.js
@@ -1,1 +1,21 @@
 "use strict";
+
+const form = document.querySelector("form");
+
+const validateForm = () => {
+    var nom = document.getElementById("last-name");
+    // Logique pour vérifier le formulaire ici
+    if (nom.value == "") {
+        alert("Mettez votre nom.");       
+        document.querySelector("label[for='last-name'] span.error-msg").innerHTML = 'Veuillez entrer votre nom';
+        nom.focus();
+        return false;
+    }
+    return true;
+};
+
+form.addEventListener("submit", (event) => {
+    if (!validateForm()) {
+        event.preventDefault();
+    }
+});
