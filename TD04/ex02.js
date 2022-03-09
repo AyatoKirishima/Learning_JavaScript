ce6dcf67549735253e6356c81636bb8e042c0b97
Blue <leanefelis@gmail.com>
2022-03-09 11:05:07 +0100

    region select ok :o:

--- TD04/ex02.js
+++ TD04/ex02.js
@@ -1,30 +1,45 @@
 "use strict"
-
-// function getRegions() {
-
-// }
-
 // Add element to a select
-// get reference to select element
-var sel = document.getElementById('region');
+// // get reference to select element
+// var sel = document.getElementById('region');
 
-// create new option element
-var opt = document.createElement('option');
+// // create new option element
+// var opt = document.createElement('option');
 
-// create text node to add to option element (opt)
-opt.appendChild(document.createTextNode('New Option Text'));
+// // create text node to add to option element (opt)
+// opt.appendChild(document.createTextNode('New Option Text'));
 
-// set value property of opt
-opt.value = 'option value';
+// // set value property of opt
+// opt.value = 'option value';
 
-// add opt to end of select box (sel)
-sel.appendChild(opt);
+// // add opt to end of select box (sel)
+// sel.appendChild(opt);
 
 
 fetch("https://geo.api.gouv.fr/regions").then(response => {
     console.log(response)
     return response.json();
 }).then(result => {
-    console.log(result.data);
+    let i = 0;
+    result.forEach(element => {
+        // get reference to select element
+        var sel = document.getElementById('region');
+
+        // create new option element
+        var opt = document.createElement('option');
+
+        // create text node to add to option element (opt)
+        opt.appendChild(document.createTextNode(result[i].nom));
+
+        // set value property of opt
+        opt.value = result[i].code;
+
+        // add opt to end of select box (sel)
+        sel.appendChild(opt);
+
+        i++;
+    });
+    console.log(result);
+
     // document.querySelector("select[id='region']").innerHTML = result.data;
 })
