2a9284bc3f8859751bb5fb85bc2e924f4ed118b2
Blue <leanefelis@gmail.com>
2022-03-09 09:39:48 +0100

    générer cat fact

---
+++ TD04/ex01.js
@@ -0,0 +1,15 @@
+"use strict"
+
+let element = document.querySelector("button");
+// console.log(element);
+element.addEventListener('click', getCatFact);
+// ne pas mettre de () car sinon cela pointe le return de la fonction
+
+function getCatFact() {
+    fetch("https://meowfacts.herokuapp.com").then(response => {
+        return response.json();
+    }).then(result => {
+        console.log(result.data);
+        document.querySelector("p[id='result']").innerHTML = result.data;
+    })
+}
