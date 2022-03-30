4bacb67ba9eb6443a02184193a5cfcb650808895
Blue <leanefelis@gmail.com>
2022-03-30 09:05:15 +0200

    ajout td eval

---
+++ TDEval/login.js
@@ -0,0 +1,13 @@
+"use strict"
+
+// Login funct
+function login() {
+    var element = document.getElementById("login-modal");
+    element.style.display = null;
+}
+
+function closeLogin() {
+    var element = document.getElementById("login-modal");
+    element.style.display = "none";
+}
+// End Login funct
