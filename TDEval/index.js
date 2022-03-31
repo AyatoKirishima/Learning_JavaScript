5c58f3c3d1f3a1da5511da821bcb439b47349917
Blue <leanefelis@gmail.com>
2022-03-31 22:35:32 +0200

    k everything is fine
    100% du franglais là dedans j'vais dormir adieu

--- TDEval/index.js
+++ TDEval/index.js
@@ -1,17 +1,54 @@
-"use strict"
+"use strict";
+
+// SearchBar
+function mySearchFunction() {
+    // Declare variables
+    let input, filter, ul, listePoke, item, i, txtValue;
+    // User Input
+    input = document.getElementById("poke-filter");
+    // Filter, makes search not case sensitive
+    filter = input.value.toUpperCase();
+    // Grabs the parent element by id
+    ul = document.getElementById("poke-list");
+    // Individual item on list
+    listePoke = ul.getElementsByTagName("li");
+
+    // Treats lists items like an array, where each item can be accessed through it's index
+    for (i = 0; i < listePoke.length; i++) {
+        item = listePoke[i];
+        // Iterate over each list item to see if the value of the input, ignoring  case, matches the inner text or inner html of the item.
+        txtValue = item.textContent || item.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            // Displays list items that are a match, and nothing if no match
+            listePoke[i].style.display = "";
+        } else {
+            listePoke[i].style.display = "none";
+        }
+    }
+}
+// End SearchBar
+
 
 /*
 * Par la magie du getElementById, cela selectionne bien l'élément
-* Cependant pas possible de le manipuler comme avec un query selector
+* Cependant pas possible de le manipuler comme avec un query selector :(
 * let pokeSelect = document.getElementById("poke-list");
 * console.log(pokeSelect);
 * console.log(pokeSelect.firstElementChild);
+* Aucune idée pourquoi
  */
 
-let pokeSelect = document.querySelector("ol[id='poke-list']");
-while (pokeSelect.firstChild) {
-    pokeSelect.removeChild(pokeSelect.firstChild);
+let list = document.querySelector("ol[id='poke-list']");
+
+function clearList() {
+    // looping through each child of the search results list and remove each child
+    while (list.firstChild) {
+        list.removeChild(list.firstChild)
+    }
 }
+
+// clear the actual list in the html
+clearList();
 
 fetch("https://pokeapi.co/api/v2/pokemon?limit=151&offset=0").then(response => {
     return response.json();
@@ -23,27 +60,140 @@ fetch("https://pokeapi.co/api/v2/pokemon?limit=151&offset=0").then(response => {
     * Sinon on obtient un "undefined"
      */
     console.log(result.results.length);
-    let maxLengthArrayPoke = result.results.length;
+    let max = result.results.length;
 
 
-    function afficherPoke() {
-        let elementListePoke = document.createElement("li");
-        console.log(capitalizeFirstLetter(result.results[i].name));
-        let namePoke = capitalizeFirstLetter(result.results[i].name);
-        elementListePoke.id = result.results[i].name;
-        elementListePoke.innerHTML = namePoke;
-        //elementListePoke.onclick = update;
+    for (let i = 0; i < max; i++) {
+        // supprimer décalage entre JSON et numéro poke
+        let j = i + 1;
 
-        pokeSelect.appendChild(elementListePoke);
-    }
+        /*
+        * Ajout des <> infos des pokemons dans la poke-list
+         */
+        function creaListePoke() {
+            let listePoke = document.createElement('li');
+            listePoke.id = result.results[i].name;
+            listePoke.value = j;
+            listePoke.onclick = update;
+            list.appendChild(listePoke);
+            let imgPoke = document.createElement('img');
+            imgPoke.onerror = "this.onerror=null;this.src='assets/poke-icons/0.png';"
+            imgPoke.src = "assets/poke-icons/" + j + ".png"
+            listePoke.appendChild(imgPoke);
+            if (j < 10) {
+                let nbPokemon = "00" + j;
+                listePoke.insertAdjacentHTML("beforeEnd", nbPokemon + " - " + capitalizeFirstLetter(result.results[i].name));
+            }
+            else if (j < 100) {
+                let nbPokemon = "0" + j;
+                listePoke.insertAdjacentHTML("beforeEnd", nbPokemon + " - " + capitalizeFirstLetter(result.results[i].name));
+            }
+            else {
+                let nbPokemon = j;
+                listePoke.insertAdjacentHTML("beforeEnd", nbPokemon + " - " + capitalizeFirstLetter(result.results[i].name));
+            }
+        }
+        creaListePoke();
 
-    let i = 0;
-    for (i = 0; i < maxLengthArrayPoke; i++) {
-        afficherPoke();
+        function clearActive() {
+            let olPokeList = document.querySelector("ol[id='poke-list']");
+            if (olPokeList.hasChildNodes) {
+                let olPokeListChildren = olPokeList.children;
+                for (let i = 0; i < olPokeListChildren.length; i++) {
+                    olPokeListChildren[i].setAttribute('class', '');
+                }
+            }
+        }
+
+        function update() {
+            clearActive();
+            this.setAttribute('class', 'active');
+            let spanTitle = document.querySelector("span[id='poke-title']");
+
+            if (j < 10) {
+                let nbPokemon = "00" + j;
+                spanTitle.innerHTML = "#" + nbPokemon + " - " + capitalizeFirstLetter(result.results[i].name);
+            }
+            else if (j < 100) {
+                let nbPokemon = "0" + j;
+                spanTitle.innerHTML = "#" + nbPokemon + " - " + capitalizeFirstLetter(result.results[i].name);
+            }
+            else {
+                let nbPokemon = j;
+                spanTitle.innerHTML = "#" + nbPokemon + " - " + capitalizeFirstLetter(result.results[i].name);
+            }
+
+            let detailsPoke = document.querySelector("div[id='poke-details']");
+            detailsPoke.firstElementChild.firstElementChild.src = "assets/poke-img/" + j + ".png";
+            let iconPoke = document.querySelector("h1");
+            iconPoke.firstElementChild.src = "assets/poke-icons/" + j + ".png";
+
+            fetch("https://pokeapi.co/api/v2/pokemon/" + j + "/").then(response => {
+                return response.json();
+            }).then(result2 => {
+
+                let typesPoke = document.querySelector("div[id='poke-types']");
+                let max = result2.types.length;
+                console.log("max : " + max);
+
+                // Clear all types (sinon ça se stack à l'infini)
+                while (typesPoke.firstChild) {
+                    typesPoke.removeChild(typesPoke.firstChild);
+                }
+
+                let i = 0;
+                for (i = 0; i < max; i++) {
+                    let imgTypesPoke = document.createElement('img');
+                    imgTypesPoke.onerror = "this.onerror=null;this.src='assets/poke-icons/0.png';"
+                    imgTypesPoke.src = "assets/poke-types//" + result2.types[i].type.name + ".gif"
+                    typesPoke.appendChild(imgTypesPoke);
+                }
+
+                let poidsPoke = document.querySelector("span[id='poke-weight']");
+                // console.log(poidsPoke);
+                // console.log(result2.weight);
+                poidsPoke.innerHTML = result2.weight;
+
+                let taillePoke = document.querySelector("span[id='poke-height']");
+                taillePoke.innerHTML = result2.height;
+
+                let valeurVitesse = result2.stats[5].base_stat;
+                let vitesse = document.querySelector("li[id = 'poke-speed'] .progress-bar");
+                vitesse.style = "width: " + valeurVitesse + "px";
+                vitesse.innerHTML = valeurVitesse;
+
+                let valeurHp = result2.stats[0].base_stat;
+                let hp = document.querySelector("div[class='progress-bar']");
+                hp.style = "width: " + valeurHp + "px";
+                hp.innerHTML = valeurHp;
+
+                let valeurAttaque = result2.stats[1].base_stat;
+                let attaque = document.querySelector("li[id = 'poke-attack'] .progress-bar");
+                attaque.style = "width: " + valeurAttaque + "px";
+                attaque.innerHTML = valeurAttaque;
+
+                let valeurDefense = result2.stats[2].base_stat;
+                let defense = document.querySelector("li[id = 'poke-defense'] .progress-bar");
+                defense.style = "width: " + valeurDefense + "px";
+                defense.innerHTML = valeurDefense;
+
+                let valeurAttaqueSpe = result2.stats[3].base_stat;
+                let attaqueSpe = document.querySelector("li[id = 'poke-special-attack'] .progress-bar");
+                attaqueSpe.style = "width: " + valeurAttaqueSpe + "px";
+                attaqueSpe.innerHTML = valeurAttaqueSpe;
+
+                let valeurDefenseSpeciale = result2.stats[4].base_stat;
+                let defenseSpeciale = document.querySelector("li[id = 'poke-special-defense'] .progress-bar");
+                defenseSpeciale.style = "width: " + valeurDefenseSpeciale + "px";
+                defenseSpeciale.innerHTML = valeurDefenseSpeciale;
+
+            })
+
+        }
+
     }
 })
 
-// Mettre la première lettre en majuscule
 function capitalizeFirstLetter(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
 }
